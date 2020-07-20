4edd548756dad45d6099471a474569200489700c
Valentin Malissen <keisuke.tk.fd@gmail.com>
2020-07-19 21:39:13 -0400

    Initial Commit

---
+++ UI-ECommerceApp/UI_ECommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_ECommerceAppApp.swift
+//  UI-ECommerceApp
+//
+//  Created by Valentin Malissen on 2020-07-19.
+//
+
+import SwiftUI
+
+@main
+struct UI_ECommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
